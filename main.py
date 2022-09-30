7a2db89a4dcac3eec34566bfef30e59e5393954b
Bimbarabambum <97984580+Bimbarabambom00@users.noreply.github.com>
2022-09-30 17:30:48 +0200

    First keyboard bindings

--- main.py
+++ main.py
@@ -1,10 +1,29 @@
 import pygame,sys
+import os
 from pygame.locals import *
 surface = pygame.display.set_mode((1280,720))
 surface.fill((55,55,55))
-pygame.display.set_caption("Game Construction")
+pygame.display.set_caption("Game Python")
+
+class PlayerSprite:
+    position_left = 0
+    position_top = 0
+    width = 40
+    height = 40
+
 while True:
     for event in pygame.event.get():
+        if event.type == pygame.KEYUP:
+            os.system('cls')
+            print(event.key)
+            if event.key == 97:
+                print("← LEFT_A")
+            if event.key == 100:
+                print("→ RIGHT_D")
+            if event.key == 115:
+                print("↓ DOWN_S")
+            if event.key == 119:
+                print("↑ UP_W")
         if event.type == QUIT:
             pygame.quit()
             sys.exit()
